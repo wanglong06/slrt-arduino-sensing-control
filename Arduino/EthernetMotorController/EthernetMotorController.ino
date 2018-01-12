02b7662457d98240231a045f6e51c8af8f2bddd8
Long Wang <wanglong06@gmail.com>
2018-01-12 15:28:35 -0600

    analog write being tested

--- Arduino/EthernetMotorController/EthernetMotorController.ino
+++ Arduino/EthernetMotorController/EthernetMotorController.ino
@@ -30,8 +30,9 @@
 int chipSelectPin1=10;
 int chipSelectPin2=9;
 int chipSelectPin3=8;
-const unsigned int COUNT_PER_TURN[3] = {256,256,256}; // Encoder spec - counts per turn
-const unsigned int QUAD_MODE[3] = {4,4,4}; // Encoder spec - Quadrature mode
+const unsigned int COUNT_PER_TURN[] = {256,256,256}; // Encoder spec - counts per turn
+const unsigned int QUAD_MODE[] = {4,4,4}; // Encoder spec - Quadrature mode
+const unsigned int GEAR_RATIO[] = {3,3,3}; // gear head spec - 5:1
 //*****************************************************  
 
 //*****************************************************  
@@ -56,24 +57,32 @@ EthernetUDP Udp;
 //*****************************************************  
 
 // time and test value
-unsigned long Time = 0; // 
-float value = 0;
+unsigned long TimeIdx = 0; // 
 
 //*****************************************************  
 // Declare variables needed for PID controller
 bool enablePID = false;
 double motorJointRad[] = {0.0, 0.0, 0.0};
 double desiredJointRad[] = {0.0, 0.0, 0.0};
-double controlOuput[] = {0.0, 0.0, 0.0};
+double controlOutput[] = {0.0, 0.0, 0.0}; // this is the analog voltage set
+const double CONTROL_MAX = 5.0;
+unsigned int controlWrite8Bit[3] = {0,0,0}; // this is the analog 8-bit number to write
+const unsigned int SABER_T_BIAS[] = {128, 128, 128}; // the middle point of sabertooth voltage.
 double Kp[] = {2,2,2};
 double Ki[] = {0,0,0};
 double Kd[] = {0,0,0};
 // Specify the links and initial tuning parameters (PID Gains)
 // PID joint1PID(&Input, &Output, &Setpoint, Kp_gain, Ki_gain, Kd_gain, DIRECT);
-PID joint1PID(motorJointRad, controlOuput, desiredJointRad, Kp[0], Ki[0], Kd[0], DIRECT);
-PID joint2PID(motorJointRad+1, controlOuput+1, desiredJointRad+1, Kp[1], Ki[1], Kd[1], DIRECT);
-PID joint3PID(motorJointRad+2, controlOuput+2, desiredJointRad+2, Kp[2], Ki[2], Kd[2], DIRECT);
+PID joint1PID(motorJointRad, controlOutput, desiredJointRad, Kp[0], Ki[0], Kd[0], DIRECT);
+PID joint2PID(motorJointRad+1, controlOutput+1, desiredJointRad+1, Kp[1], Ki[1], Kd[1], DIRECT);
+PID joint3PID(motorJointRad+2, controlOutput+2, desiredJointRad+2, Kp[2], Ki[2], Kd[2], DIRECT);
 //*****************************************************  
+
+//*****************************************************  
+// The Analog write
+int joint1AnalogWrite = 3;      // LED connected to digital pin 9
+int joint2AnalogWrite = 5;      // LED connected to digital pin 9
+int joint3AnalogWrite = 6;      // LED connected to digital pin 9
 
 //*****************************************************
 void setup() 
@@ -83,6 +92,14 @@ void setup()
   joint1PID.SetMode(AUTOMATIC);
   joint2PID.SetMode(AUTOMATIC);
   joint3PID.SetMode(AUTOMATIC);
+  joint1PID.SetOutputLimits(-CONTROL_MAX,CONTROL_MAX);
+  joint2PID.SetOutputLimits(-CONTROL_MAX,CONTROL_MAX);
+  joint3PID.SetOutputLimits(-CONTROL_MAX,CONTROL_MAX);
+  
+  // setup the analog write
+  pinMode(joint1AnalogWrite, OUTPUT);
+  pinMode(joint2AnalogWrite, OUTPUT);
+  pinMode(joint3AnalogWrite, OUTPUT);  
   
   // setup for Ethernet UDP
   Ethernet.begin(mac, ip);
@@ -112,8 +129,9 @@ void loop()
   // Encoder reading and motor angle calculation
     long encoderValue[3];
     for (int i = 0; i < 3; i++) {
-      encoderValue[i] = getEncoderValue(i); 
-      motorJointRad[i] = (float)encoderValue[i]/(COUNT_PER_TURN[i]*QUAD_MODE[i]);
+      encoderValue[i] = getEncoderValue(i+1); 
+      motorJointRad[i] = (float)encoderValue[i]/
+      (COUNT_PER_TURN[i]*QUAD_MODE[i]*GEAR_RATIO[i]);
     }
   
   // Receive command from master 
@@ -134,17 +152,47 @@ void loop()
     // send a reply to the IP address and port that sent us the packet we received
     double DataToSend[] = {
       motorJointRad[0], motorJointRad[1], motorJointRad[2],
-      controlOuput[0], controlOuput[1], controlOuput[2]
+      controlOutput[0], controlOutput[1], controlOutput[2]
     };
     pack_UDP_message(SendBuffer,DataToSend);
     Udp.beginPacket(sendIP, sendPort);
     Udp.write(SendBuffer,sizeof(DataToSend));
     Udp.endPacket();
-  
+
   // Apply the computed control action to robot.
+    //  convert controlOutput to analog write value
+    for (int i=0; i<3; i++){
+      controlWrite8Bit[i] = controlOutput[i] / CONTROL_MAX * 128 + SABER_T_BIAS[i];
+    }
+    
+    //  write to the pin
+      //    analogWrite(joint1AnalogWrite,controlWrite8Bit[0]);
+    analogWrite(joint2AnalogWrite,controlWrite8Bit[1]);
+    analogWrite(joint3AnalogWrite,controlWrite8Bit[2]);
+
   
-    delay(10); 
- 
+  // Display the status for debugging purposes
+    //    Serial.print("q1: ");    
+    //    Serial.print(motorJointRad[0]);
+    Serial.print("  q2: ");    
+    Serial.print(motorJointRad[1]);
+    //    Serial.print("  q3: ");    
+    //    Serial.print(motorJointRad[2]);
+    //    Serial.print("  u1: ");  
+    //    Serial.print(controlOutput[0]);
+    Serial.print("  u2: ");  
+    Serial.print(controlOutput[1]);
+    //    Serial.print("  u3: ");  
+    //    Serial.print(controlOutput[2]);
+    Serial.print("  u2[8bit]:");
+    Serial.print(controlWrite8Bit[1]);
+    Serial.print("\r\n");
+    TimeIdx = TimeIdx + 1;
+  
+  
+  // Loop delay
+    delay(10);
+   
 }//end loop
 
 
